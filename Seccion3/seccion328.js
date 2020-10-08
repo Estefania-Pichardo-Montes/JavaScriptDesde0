f02d055e210fa213d9cb031d410772d79ef9a0f3
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-10-08 10:04:27 -0500

    Leccion 28-Objeto Number

---
+++ Seccion3/seccion328.js
@@ -0,0 +1,16 @@
+var a=10.452355;
+
+// console.log(a.toFixed(5));
+
+// a=a.toString();
+// console.log(a);
+console.log(a);
+console.log(a.toPrecision(3));
+
+a=10;
+
+console.log(a+10);
+
+var b= new Number("20");
+console.log(b);
+console.log(b.valueOf());
