37a5b750a92948052dc94080b01bcec2b255765e
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-10-09 11:27:39 -0500

    Seccion4-Leccion46 JSON

---
+++ Seccion4/seccion446.js
@@ -0,0 +1,21 @@
+var objetoJS={
+	nombre:"Estefania",
+	edad:21,
+	imprimir: function(){
+		console.log(this.nombre, this.edad);
+	}
+};
+
+console.log("Objeto literal", objetoJS);
+
+var jsonString= JSON.stringify(objetoJS);
+
+console.log(jsonString);
+
+var objetoDesdeJson=JSON.parse(jsonString);
+
+console.log(objetoDesdeJson);
+
+console.log(objetoDesdeJson.nombre);
+
+console.log(objetoDesdeJson.edad);
