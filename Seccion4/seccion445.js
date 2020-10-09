89f39e051d95404a81a3f5c53b356834fa921626
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-10-09 11:27:07 -0500

    Seccion4-leccion45 Switch

---
+++ Seccion4/seccion445.js
@@ -0,0 +1,16 @@
+var mes=2;
+
+switch(mes){
+
+case (10>5)?40:1: console.log("Enero");
+break;
+
+case 2: console.log("Febrero");
+break;
+
+case 3: console.log("Marzo");
+break;
+
+default: console.log("Cualquier otro mes");
+
+}
