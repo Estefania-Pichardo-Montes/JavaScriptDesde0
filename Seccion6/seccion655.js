7828862899c0dbd66d7c5b6293b6163e43b55536
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-10-10 19:53:39 -0500

    Seccion6-Leccion55 Modo estricto

---
+++ Seccion6/seccion655.js
@@ -0,0 +1,14 @@
+(function(){
+	"use strict";
+	function getNombre(){
+	
+		nombre2="Juan";
+		return nombre2;	
+	}
+
+
+	var nombre= "Estefania";
+	console.log(nombre);
+	console.log(getNombre());
+
+})();
