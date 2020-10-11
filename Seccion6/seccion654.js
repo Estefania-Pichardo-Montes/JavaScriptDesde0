ecf250a97c35479c3abf8594c5db9f7175a89b4b
Estefania-Pichardo-Montes <72160590+Estefania-Pichardo-Montes@users.noreply.github.com>
2020-10-10 19:53:04 -0500

    Seccion6-Leccion54 Cajas de dialogo

---
+++ Seccion6/seccion654.js
@@ -0,0 +1,15 @@
+setInterval(function(){
+	console.log("segundo...");
+},1000);
+
+
+
+
+
+alert("Hola Mundo");
+
+var acepto= confirm("Esta seguro que desea borrar algo");
+console.log(acepto);
+
+var acepto2= prompt("Ingrese su nombre","nombre");
+console.log(nombre);
